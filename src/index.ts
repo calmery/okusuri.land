7d2bb40b8d23abc6e7b157b40b8afe821d8f9dd1
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2020-10-08 01:35:04 +0900

    :pill: の絵文字を返すように変更する

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as http from "http";
 
 const httpServer = http.createServer((request, response) => {
   console.log(new Date(), "Received request for", request.url);
-  response.writeHead(200);
+  response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  response.write("<marquee>💊</marquee>");
   response.end();
 });
 
