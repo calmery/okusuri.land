6af99f568ff624a8c2a0871adca3e393454d781f
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-06 05:51:21 +0900

    useDepartments の型を API のレスポンスに合わせて修正する

--- src/hooks/useDepartments.ts
+++ src/hooks/useDepartments.ts
@@ -3,18 +3,16 @@ import { Department } from "~/types/Department";
 import { ApiResponse, get } from "~/utils/api";
 
 const getDepartments = async () => {
-  const { data } = await get<ApiResponse<{ departments: Department[] }>>(
-    "/departments"
-  );
+  const { data } = await get<ApiResponse<Department[]>>("/departments");
 
   return data;
 };
 
 export const useDepartments = () => {
-  const { data, error } = useSWR<{ departments: Department[] }, Error>(
+  const { data, error } = useSWR<Department[], Error>(
     "/departments",
     getDepartments
   );
 
-  return { departments: data?.departments, error };
+  return { departments: data, error };
 };
