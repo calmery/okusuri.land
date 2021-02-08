f225eaf7d00a2f1d231788c538e123be60351276
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-08 12:53:27 +0900

    本番環境以外で URL に `?debug` を付けたとき、右クリックを使えるようにする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "~/domains";
@@ -8,12 +9,21 @@ import "~/utils/sentry";
 import "~/styles/globals.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const { query } = useRouter();
+  const { debug } = query;
+
   useEffect(() => {
     document.oncontextmenu = () => {
+      if (process.env.NODE_ENV !== "production" && debug !== undefined) {
+        return true;
+      }
+
       alert("右クリックは禁止です！");
       return false;
     };
+  }, [debug]);
 
+  useEffect(() => {
     firebase.auth().onAuthStateChanged(async () => {
       await store.dispatch(actions.refreshToken());
       await store.dispatch(actions.refreshProfile());
