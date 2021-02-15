8517ce6c786406f9949a6327e27442d9f9ac47d6
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-16 05:21:17 +0900

    login.htm.tsx を okusuri.htm.tsx に、ログイン済みのときはマイページに遷移する

--- src/pages/okusuri.htm.tsx
+++ src/pages/okusuri.htm.tsx
@@ -1,15 +1,24 @@
-import React, { useCallback, useLayoutEffect } from "react";
+import { useRouter } from "next/router";
+import React, { useCallback, useEffect, useLayoutEffect } from "react";
 import { Page } from "~/components/Page";
-import { useDispatch } from "~/domains";
-import { actions } from "~/domains/authentication";
+import { useDispatch, useSelector } from "~/domains";
+import { actions, selectors } from "~/domains/authentication";
 import html from "~/static/login.html";
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
+  const { push } = useRouter();
+  const profile = useSelector(selectors.profile);
 
   const handleClickLogInButton = useCallback(() => {
     dispatch(actions.authenticate());
   }, []);
+
+  useEffect(() => {
+    if (profile) {
+      push(`/~${profile.screenName}/index.htm`);
+    }
+  }, [profile]);
 
   useLayoutEffect(() => {
     const loginButton = document.getElementById("login");
