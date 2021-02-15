8a3562614a288f51f729d2367ed94ceafae3d291
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-16 05:25:43 +0900

    /[screenName]/index.htm にリダイレクトする

--- src/pages/[screenName].tsx
+++ src/pages/[screenName].tsx
@@ -1,1 +1,14 @@
-export { default, getServerSideProps } from "./[screenName]/index.htm";
+import { GetServerSideProps, NextPage } from "next";
+
+const Noop: NextPage = () => null;
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  res,
+}) => {
+  res.statusCode = 301;
+  res.setHeader("Location", `/${query.screenName as string}/index.htm`);
+  return { props: {} };
+};
+
+export default Noop;
