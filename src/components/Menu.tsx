b85fd83ecb9ee3244387abef5d9b9f95cc4091ed
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-16 04:33:52 +0900

    サイドメニューが動作するようにする

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,6 @@
 import { css } from "@emotion/react";
+import { useRouter } from "next/router";
+import { useCallback, useLayoutEffect } from "react";
 import html from "~/static/menu.html";
 
 const container = css`
@@ -7,5 +9,32 @@ const container = css`
 `;
 
 export const Menu: React.FC = () => {
+  const { push } = useRouter();
+
+  const handleClickLink = useCallback((event: MouseEvent) => {
+    const a = event.currentTarget as HTMLAnchorElement | null;
+
+    if (!a) {
+      return;
+    }
+
+    if (window.location.origin === a.origin) {
+      event.preventDefault();
+      push(a.pathname);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    const links = document.querySelectorAll("a");
+
+    links.forEach((link) => link.addEventListener("click", handleClickLink));
+
+    return () => {
+      links.forEach((link) =>
+        link.removeEventListener("click", handleClickLink)
+      );
+    };
+  }, []);
+
   return <div css={container} dangerouslySetInnerHTML={{ __html: html }} />;
 };
