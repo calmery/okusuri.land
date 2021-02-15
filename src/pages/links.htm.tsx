2e2c84657d515f2876af9e5b7fe50d8f8b8e465a
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-02-16 04:13:01 +0900

    Links ページを追加する

---
+++ src/pages/links.htm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Page } from "~/components/Page";
+import html from "~/static/links.html";
+
+const Links: React.FC = () => {
+  return <Page>{html}</Page>;
+};
+
+export default Links;
